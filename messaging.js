4c6b0d1ab75dfbdc34a23c2bc8266831088f9df1
Calvin Kinyanjui <158546364+CalKK@users.noreply.github.com>
2026-05-06 21:48:07 +0300

    Revise email content for Master of Ceremonies application
    Updated email template and subject for application.

--- messaging.js
+++ messaging.js
@@ -6,17 +6,15 @@ const emailTemplate = `Dear Sir/Madam,
 
 I trust this message finds you well.
 
-I go by the name of Calvin Kinyanjui, 5th Year Electrical & Electronics Engineering Student at Strathmore University awaiting graduation in August 2026. I write this email with the view to lodge an application seeking a job opportunity at [Institution]
+I go by the name of Calvin Kinyanjui. I write this email with the view to lodge an application seeking a free Master of Ceremonies service at [Institution] corporate events.
 
-I also wanted to take this time to share with you my professional portfolio.
-
-Kindly find attached below my resume and recommendation letter.
+Kindly find attached below my contact details card.
 
 In case of any queries, don't hesitate to reach out.
 
 Thank you & Kind Regards!`;
 
-const defaultSubject = 'Job Application — Calvin Kinyanjui, Electrical & Electronics Engineering';
+const defaultSubject = 'Master of Ceremonies for your Corporate Events for free - Calvin Kinyanjui ';
 
 /**
  * Create a Nodemailer transporter using Gmail SMTP
